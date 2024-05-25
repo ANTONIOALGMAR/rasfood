9a1804ba2d91bb7b433a145f0186e2e45dbdd02f
antonio <antonio@MacBook-Pro-de-antonio.local>
2024-05-25 19:11:50 -0300

    init

---
+++ src/main/java/br/com/rasmoo/restarurante/entity/Categoria.java
@@ -0,0 +1,39 @@
+package br.com.rasmoo.restarurante.entity;
+
+public class Categoria {
+
+    private Integer id;
+
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
